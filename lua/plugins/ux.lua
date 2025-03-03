9bf12877033ca9fee9a98289cd8c66eae9f5dc00
Geoff Tearle <18473961+GeoffTearle@users.noreply.github.com>
2025-03-04 08:46:43 +1300

    small refactor

--- lua/plugins/ux.lua
+++ lua/plugins/ux.lua
@@ -1,4 +1,79 @@
 return {
+  -- {
+  --   "rachartier/tiny-glimmer.nvim",
+  --   event = "VeryLazy",
+  --   opts = {
+  --     -- your configuration
+  --   },
+  -- },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    dependencies = {
+      "TheGLander/indent-rainbowline.nvim",
+    },
+    main = "ibl",
+    config = function()
+      local opts = require("indent-rainbowline").make_opts()
+      require("ibl").setup(opts)
+    end,
+  },
+  -- save my last cursor position
+  {
+    "ethanholz/nvim-lastplace",
+    config = function()
+      require("nvim-lastplace").setup({
+        lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+        lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
+        lastplace_open_folds = true,
+      })
+    end,
+  },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+    ---@type NoiceConfig
+    opts = {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+        documentation = {
+          view = "hover",
+          ---@type NoiceViewOptions
+          opts = {
+            -- lang = "markdown",
+            -- replace = true,
+            -- render = "plain",
+            -- format = { "{message}" },
+            win_options = { concealcursor = "nvic", conceallevel = 0, wrap = true },
+            -- size = { row = 9999, col = 9999 },
+          },
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+    },
+  },
   {
     "LZDQ/nvim-autocenter",
     version = "*",
